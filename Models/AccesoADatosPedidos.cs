2c6d7c912fcf9c14f185602e351ec15570605bc6
Martina Gutierrez <martugutierrez166@gmail.com>
2025-09-26 18:14:41 -0300

    Se creó la clase AccesoADatosPedidos con dos métodos "Obtener" y "Guardar"

---
+++ Models/AccesoADatosPedidos.cs
@@ -0,0 +1,28 @@
+using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using EspacioPedido;
+
+namespace EspacioAccesoADatosPedidos
+{
+    class AccesoADatosPedidos()
+    {
+        public static List<Pedido> Obtener(string archivo)
+        {
+            List<Pedido> listaPedidosJson = new();
+            string textoJson = File.ReadAllText(archivo);
+            listaPedidosJson = JsonSerializer.Deserialize<List<Pedido>>(textoJson);
+
+            return listaPedidosJson;
+        }
+
+        public static void Guardar(List<Pedido> pedidos)
+        {
+            string textoJson = JsonSerializer.Serialize(pedidos);
+            string miArchivo = "Archivos/Pedidos.json";
+
+            File.WriteAllText(miArchivo, textoJson);
+        }
+    }
+}
