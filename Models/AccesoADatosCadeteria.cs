b7b3f4793cb4f4e3d906420b5e2e852329dd9897
Martina Gutierrez <martugutierrez166@gmail.com>
2025-09-26 18:04:02 -0300

    Se creó la clase AccesoADatosCadeteria con un método "Obtener"

---
+++ Models/AccesoADatosCadeteria.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Text.Json;
+using EspacioCadeteria;
+
+namespace EspacioAccesoADatosCadeteria
+{
+    class EspacioAccesoADatosCadeteria
+    {
+        public static Cadeteria Obtener(string archivo)
+        {
+            Cadeteria cadeteria;
+            string textoJson = File.ReadAllText(archivo);
+            cadeteria = JsonSerializer.Deserialize<Cadeteria>(textoJson);
+
+            return cadeteria;
+        }
+    }
+}
