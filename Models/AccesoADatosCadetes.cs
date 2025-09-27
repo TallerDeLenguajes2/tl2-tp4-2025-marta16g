2bf129a0fdcfc73dcd8ad21925798ef2c90dbefc
Martina Gutierrez <martugutierrez166@gmail.com>
2025-09-27 11:33:39 -0300

    El parámetro de Obtener() pasó a ser variable inicializada dentro del método

--- Models/AccesoADatosCadetes.cs
+++ Models/AccesoADatosCadetes.cs
@@ -7,8 +7,9 @@ namespace EspacioAccesoADatosCadetes
 {
     class AccesoADatosCadetes
     {
-        public List<Cadete> Obtener(string archivo)
+        public List<Cadete> Obtener()
         {
+            string archivo = "Archivos/Cadetes.json";
             List<Cadete> listaCadetesJson = new();
             string textoJson = File.ReadAllText(archivo);
             listaCadetesJson = JsonSerializer.Deserialize<List<Cadete>>(textoJson);
